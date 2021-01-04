9eef8ea748bd116ee36cdfc9e477f14600f2ff44
Semjon Krolzik <semjon.krolzik@maihiro.com>
2021-01-04 12:01:58 +0100

    overworked content

--- docu/src/gatsby-theme-carbon/templates/Homepage.js
+++ docu/src/gatsby-theme-carbon/templates/Homepage.js
@@ -5,10 +5,20 @@ import { calloutLink } from './Homepage.module.scss';
 
 import Tools from '../../images/tools.jpg';
 
+const FirstLeftText = () => '<p>Any Test</p>';
+
+const FirstRightText = () => '<p>Another Text</p>';
+
+const SecondLeftText = () => '';
+
+const SecondRightText = () => '';
+
 const BannerText = () => <h1>SAP Commerce Workplace Tools</h1>;
 
 const customProps = {
   Banner: <HomepageBanner renderText={BannerText} image={Tools} />,
+  FirstCallout: '     ',
+  SecondCallout: '',
 };
 
 // spreading the original props gives us props.children (mdx content)
